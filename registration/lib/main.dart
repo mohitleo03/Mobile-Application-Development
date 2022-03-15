d62dbe791268b40eb11976babba0b5c816254cef
mohitleo03 <electronicsmartapi@gmail.com>
2022-03-16 00:52:08 +0530

    BMI Calc app

--- registration/lib/main.dart
+++ registration/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:registration/screens/material.dart';
 
 import 'screens/registration_screen.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: registration_screen(),
+    home: FormScreen(),
     theme: ThemeData.dark(),
   ));
 }
