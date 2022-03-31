c7341069e40a8aa11f9289b2e2b4ed0aa1082c16
mohitleo03 <mohitsmalik2038@gmail.com>
2022-03-31 13:30:30 +0530

    update

--- music_app/lib/screens/list_of_songs.dart
+++ music_app/lib/screens/list_of_songs.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/utils/api_client.dart';
 
 class ListOfSongs extends StatefulWidget {
-  const ListOfSongs({ Key? key }) : super(key: key);
+  const ListOfSongs({Key? key}) : super(key: key);
 
   @override
   State<ListOfSongs> createState() => _ListOfSongsState();
@@ -9,9 +10,18 @@ class ListOfSongs extends StatefulWidget {
 
 class _ListOfSongsState extends State<ListOfSongs> {
   @override
+  void initState() {
+    // TODO: implement initState
+    ApiClient client = ApiClient();
+    client.getSongs();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Songs"),),
+      appBar: AppBar(
+        title: Text("Songs"),
+      ),
       body: Container(),
     );
   }
--- music_app/lib/utils/api_client.dart
+++ music_app/lib/utils/api_client.dart
@@ -1,9 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class ApiClient {
   getSongs() {
     String URL = "https://itunes.apple.com/search?term=jack+johnson&limit=25";
-    Future<http.Response> response = http.get(Uri.parse(URL));
-
+    Future<http.Response> future = http.get(Uri.parse(URL));
+    future.then((response) {
+      String json = response.body;
+      print("JSON $json");
+      print(json.runtimeType);
+      json = jsonDecode(json);
+      json.results
+    }).catchError((err) => print(err));
   }
+}
+
+void main() {
+  ApiClient obj = ApiClient();
+  obj.getSongs();
 }
