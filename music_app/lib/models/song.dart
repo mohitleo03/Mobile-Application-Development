53963df732b929abdd4068ed2dbcbdaef9191981
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-01 09:50:27 +0530

    updates

---
+++ music_app/lib/models/song.dart
@@ -0,0 +1,7 @@
+class Song {
+  String artistName; //artist of the song
+  String trackName; //song name
+  String image; //image URL
+  String audio; //song URL
+  Song(this.artistName, this.trackName, this.image, this.audio);
+}
--- music_app/lib/utils/api_client.dart
+++ music_app/lib/utils/api_client.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
-
+import 'dart:convert' as jsonconvert;
 import 'package:http/http.dart' as http;
+import '../models/song.dart';
 
 class ApiClient {
   getSongs() {
@@ -8,10 +8,18 @@ class ApiClient {
     Future<http.Response> future = http.get(Uri.parse(URL));
     future.then((response) {
       String json = response.body;
-      print("JSON $json");
-      print(json.runtimeType);
-      final Parsedjson = jsonDecode(json);
-      Parsedjson.results;
+      // print("JSON $json");
+      // print(json.runtimeType);
+      Map<String, dynamic> map =
+          jsonconvert.jsonDecode(json); //Json Convert into Map
+      //print("Map is $map and Map Type is ${map.runtimeType}");
+      List<Map> list = map["results"]; //get the list from map
+      List<Song> songs = list
+          .map((element) => Song(element['artistName'], element['trackName'],
+              element['artworkUrl30'], element['previewUrl']))
+          .toList(); //traverse the lsit & get one by one map
+      // and convert map into song object and song object store in a song list
+      print(songs);
     }).catchError((err) => print(err));
   }
 }
