e5c129d25bbe2a90b0643355e39a9b1ab21c04f8
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-08 20:10:17 +0530

    update

--- music_app/lib/screens/list_of_songs.dart
+++ music_app/lib/screens/list_of_songs.dart
@@ -23,23 +23,35 @@ class _ListOfSongsState extends State<ListOfSongs> {
 
   getSongsList(List<Song> Songs) {
     this.Songs = Songs;
+    setState(() {});
   }
 
   getError(dynamic error) {
     print("Error found in network call $error");
+    setState(() {});
+  }
+
+  Center _showLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  _printSong() {
+    return ListView.builder(
+      itemBuilder: (BuildContext ctx , int index) {
+          
+      },
+      itemCount: Songs.length,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Songs'),
-      ),
-      body: Container(
-          // child: ListView.builder(itemBuilder: (){
-
-          // },itemCount: ,),
-          ),
-    );
+        appBar: AppBar(
+          title: Text('Songs'),
+        ),
+        body: Container(child: Songs.isEmpty ? _showLoading() : _printSong()));
   }
 }
