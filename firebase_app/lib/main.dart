8307185a0cb80b1ee0f2bb432fc107ea5c72fce6
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-18 00:08:01 +0530

    firebase demo

--- firebase_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-Future<void> main() async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
-
-// Ideal time to initialize
-await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-//...
-}
--- firebase_demo/test/widget_test.dart
+++ firebase_demo/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testing_waves/main.dart';
+import 'package:firebase_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
