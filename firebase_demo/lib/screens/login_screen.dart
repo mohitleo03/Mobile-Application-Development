ef28a179dedccd46cb369cb0b5e62a4117bfea71
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-18 00:19:03 +0530

    update

---
+++ firebase_demo/lib/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+    );
+  }
+}
