1524e55a31ac31557165609ec7e80aec93980d1a
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-29 12:07:25 +0530

    update

---
+++ debugging_tools/android/app/src/main/kotlin/com/example/debugging_tools/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.debugging_tools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ debugging_tools/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Row(
+        children: [
+          Text("llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll",style: TextStyle(fontSize: 50),)
+        ],
+      ),
+    ),
+  ));
+}
