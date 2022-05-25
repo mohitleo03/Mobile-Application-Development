50ecdf4487fefc330c20bdd0b53eba747701d1dc
mohitleo03 <mohitsmalik2038@gmail.com>
2022-05-25 21:19:26 +0530

    Updated

--- shopping_seller_app/lib/config/routes/routes.dart
+++ shopping_seller_app/lib/config/routes/routes.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_seller_app/config/constants/AppConstants.dart';
 import 'package:shopping_seller_app/modules/screens/dashboard.dart';
-import 'package:shopping_seller_app/my_attempt/view_prod.dart';
 
 import '../../core/auth/screens/login.dart';
 import '../../core/auth/screens/register.dart';
@@ -11,9 +10,7 @@ import '../../core/auth/screens/register.dart';
 Map<String,WidgetBuilder>getRoutes() {
   return {
     RouteConstants.LOGIN:(context) => const Login(),
-    //when 
     RouteConstants.REGISTER:(context) => const Register(),
     RouteConstants.DASHBOARD:(context) => const Dashboard(),
-    // RouteConstants.VIEW_PRODUCTS:(context) => const view_products()
   };
 }
