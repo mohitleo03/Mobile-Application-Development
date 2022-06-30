1c8cec561c377934e6f75c9c54c2354e3ee64746
assignment01 <mohitsmalik2038@gmail.com>
2022-06-30 13:28:12 +0530

    update

---
+++ notification_demo/lib/notification_service.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  NotificationService._internal();
+  
+}
