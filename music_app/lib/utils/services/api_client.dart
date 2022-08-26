10d6866c969dd4a3e417c01ed8740f26e840aa94
assignment01 <mohitsmalik2038@gmail.com>
2022-08-26 11:18:54 +0530

    Update api_client.dart

--- music_app/lib/utils/services/api_client.dart
+++ music_app/lib/utils/services/api_client.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:http/http.dart' as http;
 import '/config/constants/api_path.dart';
 import 'dart:convert' as jsonconvert;
